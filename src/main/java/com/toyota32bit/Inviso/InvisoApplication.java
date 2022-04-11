ebb71e18809015135327838b203e5c3e9bbb42e5
ErenUgurlu <62996115+ErenUgurlu@users.noreply.github.com>
2022-04-11 16:21:50 +0300

    First commit empty project

---
+++ src/main/java/com/toyota32bit/Inviso/InvisoApplication.java
@@ -0,0 +1,13 @@
+package com.toyota32bit.Inviso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvisoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyota32bit/Inviso/InvisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyota32bit.Inviso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
